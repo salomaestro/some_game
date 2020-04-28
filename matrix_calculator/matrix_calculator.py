96ec5de7019571336156474599c6498490e8ee2a
salomaestro <chris10an.salomonsen@gmail.com>
2020-04-28 13:52:19 +0200

    Har implementert multiplikasjon men noe med indekseringen er feil

--- matrix_calculator/matrix_calculator.py
+++ matrix_calculator/matrix_calculator.py
@@ -1,17 +1,20 @@
 class Matrix:
     def __init__(self, elements):
         """
-        initen tester at vi har korrekt input, en liste
+        initen tester at vi har korrekt input, en liste samt den vil si noe om matrisen er kvadratisk og hvilken
+        størrelse matrisen har
         """
+
         try:
             if isinstance(elements, list):
                 self.elem = elements
         except Exception as e:
             raise e
+
         self.isquadratic = False
-        # n er antall rader m er antall søyler, altså har vi en n x m matrise
-        self.n = len(self.elem)
-        self.m = [len(row) for row in self.elem][0]
+        # m er antall rader n er antall søyler, altså har vi en n x m matrise
+        self.m = len(self.elem)
+        self.n = [len(row) for row in self.elem][0]
         if self.n == self.m:
             self.isquadratic = True
         self.size = '{} x {}'.format(self.n, self.m)
@@ -30,13 +33,27 @@ class Matrix:
         subbd = [[ j - k for j, k in zip(i, l)]for i, l in zip(self.elem, other.elem)]
         return Matrix(subbd)
 
+    def __mul__(self, other):
+        res = [[0]*self.n]*self.n
+        if self.n == other.m:
+            for i in range(self.m):
+                for j in range(other.n):
+                    for k in range(self.n):
+                        res[i][j] += self.elem[i][k] * other.elem[k][j]
+            return Matrix(res)
+
 if __name__ == "__main__":
+
     a = [[2, 2, 3], [4, 5, 6], [7, 8, 9]]
     b = [[2, 9, -5], [-1, 8, 8], [7, 2, 4]]
     c = [[1, 2], [3, 4]]
-    m1 = Matrix(a)
-    m2 = Matrix(b)
-    m3 = Matrix(c)
+    d = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
 
-    print(m1)
-    print(m1 - m2)
+    A = Matrix(a)
+    B = Matrix(b)
+    C = Matrix(c)
+    D = Matrix(d)
+
+    # print(A)
+    # print(A - B)
+    print(D * D)
