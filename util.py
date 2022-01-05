93169d2f4fa1dc08b830b5816ed8040fab803bdd
Christian Salomonsen <55956280+salomaestro@users.noreply.github.com>
2022-01-05 12:12:41 +0100

    new func uses PIL to get pixels from png

--- util.py
+++ util.py
@@ -1,8 +1,17 @@
 import pygame as pg
 from random import randint
+import numpy as np
+from PIL import Image
+from os.path import join, dirname
 
 vec = pg.math.Vector2
 
 def randvec(min_x=-10, max_x=10, min_y=-10, max_y=10):
     """ Generates a random pygame.math.Vector2 vector """
     return vec(randint(min_x, max_x), randint(min_y, max_y))
+
+def get_pix_of_img(imgname) -> np.ndarray:
+    with Image.open(imgname) as im:
+        data = np.array(im)
+    return data
+    
